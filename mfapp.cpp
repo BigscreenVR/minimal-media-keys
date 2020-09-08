dca77956702ad5eac2425851c508860567bb11da
David Petrie <davidcpetrie@gmail.com>
2020-09-08 21:03:42 +1200

    Debugging media keys - switch to EME v2 APIs.

--- mfapp.cpp
+++ mfapp.cpp
@@ -3,6 +3,7 @@
 #include <thread>
 #include <conio.h>
 #include <process.h>
+#include <propvarutil.h>
 #include <strsafe.h>
 #include <mfapi.h>
 #include <mfmediaengine.h>
@@ -75,6 +76,77 @@ public:
     }
 };
 
+IMFMediaEngineClassFactory *factory = nullptr;
+IMFMediaEngine *mediaEngine = nullptr;
+IMFMediaKeys2 *mediaKeys = nullptr;
+IMFMediaKeySession2 *mediaKeySession2 = nullptr;
+
+class MediaKeySessionNotify : public IMFMediaKeySessionNotify2 {
+    long m_cRef;
+
+public:
+    // Constructor for reactive license requests.
+    MediaKeySessionNotify() : m_cRef(0) {}
+
+    STDMETHODIMP QueryInterface(REFIID riid, void** ppv)
+    {
+        if (__uuidof(IMFMediaKeySessionNotify2) == riid)
+        {
+            *ppv = static_cast<IMFMediaKeySessionNotify2*>(this);
+        }
+        else
+        {
+            *ppv = nullptr;
+            return E_NOINTERFACE;
+        }
+
+        AddRef();
+
+        return S_OK;
+    }
+
+    STDMETHODIMP_(ULONG) AddRef()
+    {
+        return InterlockedIncrement(&m_cRef);
+    }
+
+    STDMETHODIMP_(ULONG) Release()
+    {
+        LONG cRef = InterlockedDecrement(&m_cRef);
+        if (cRef == 0)
+        {
+            delete this;
+        }
+        return cRef;
+    }
+
+    void STDMETHODCALLTYPE KeyAdded(void) {
+        Log("Key added");
+    }
+
+    void STDMETHODCALLTYPE KeyError(USHORT code, DWORD systemCode) {
+        Log("KeyError");
+    }
+
+    void STDMETHODCALLTYPE KeyMessage(BSTR destinationUrl, const BYTE *message, DWORD cb) {
+        Log("KeyMessage");
+    }
+
+    void STDMETHODCALLTYPE KeyMessage2(MF_MEDIAKEYSESSION_MESSAGETYPE eMessageType,
+        _In_opt_  BSTR destinationURL,
+        _In_reads_bytes_(cbMessage)  const BYTE *pbMessage,
+        _In_  DWORD cbMessage) {
+        Log("KeyMessage2 %d\n", eMessageType);
+
+    }
+
+    void STDMETHODCALLTYPE KeyStatusChange(void) {
+        Log("KeyStatusChange");
+    }
+};
+
+MediaKeySessionNotify *mediaKeySessionNotify = new MediaKeySessionNotify();
+
 class MediaEngineNeedKeyNotify : public IMFMediaEngineNeedKeyNotify {
     long m_cRef;
 
@@ -108,11 +180,15 @@ public:
     void STDMETHODCALLTYPE NeedKey(_In_reads_bytes_opt_(cb) const BYTE *initData, _In_ DWORD cb) {
         Log("Needkey, len: %d", cb);
         Log("If we got here, then the key system appears to be working on this version of windows - press ESC to quit");
+
+        HRESULT hr = mediaKeys->CreateSession2(MF_MEDIAKEYSESSION_TYPE_TEMPORARY, mediaKeySessionNotify, &mediaKeySession2);
+
+        if (FAILED(hr = mediaKeySession2->GenerateRequest(L"cenc", initData, cb))) {
+            Log("GenerateRequest failed: 0x%x\n", hr);
+        }
     }
 };
 
-IMFMediaEngineClassFactory *factory = nullptr;
-IMFMediaEngine *mediaEngine = nullptr;
 MediaEngineNotify *mediaEngineNotify = nullptr;
 MediaEngineNeedKeyNotify *mediaEngineNeedKeyNotify = nullptr;
 
@@ -155,8 +231,9 @@ HRESULT CreateProtectedPlaybackSession() {
         return hr;
     }
 
-    if (FAILED(hr = attributes->SetGUID(MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE, MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE_IE11))) {
-        Log("Failed to set MF_MEDIA_ENGINE_BROWSER_COMPATIBILITY_MODE 0x%x", hr);
+    
+    if (FAILED(hr = attributes->SetUINT64(MF_MEDIA_ENGINE_VIDEO_OUTPUT_FORMAT, DXGI_FORMAT_R8G8B8A8_UNORM))) {
+        Log("Failed to set MF_MEDIA_ENGINE_PLAYBACK_HWND 0x%x", hr);
         return hr;
     }
 
@@ -193,13 +270,13 @@ HRESULT CreateProtectedPlaybackSession() {
     ZeroMemory((void *)cwd, CWD_BUFFER_LEN * sizeof(wchar_t));
     DWORD cwdLen = GetCurrentDirectoryW(CWD_BUFFER_LEN, (LPWSTR)cwd);
     std::wstring cwdCdmStore(cwd, std::wstring::traits_type::length(cwd));
-    cwdCdmStore.append(L"\\cdm\\");
+    cwdCdmStore.append(L"\\cdm");
     std::wstring cwdPrivateCdmStores(cwd, cwdLen);
-    cwdPrivateCdmStores.append(L"\\cdm_private\\");
+    cwdPrivateCdmStores.append(L"\\cdm_private");
 
     BSTR defaultCdmStorePath = SysAllocStringLen(cwdCdmStore.c_str(), cwdCdmStore.size());
     BSTR inprivateCdmStorePath = SysAllocStringLen(cwdPrivateCdmStores.c_str(), cwdPrivateCdmStores.size());
-
+    
     BOOL bResult;
 
     // Create the cdm store directory.
@@ -214,16 +291,123 @@ HRESULT CreateProtectedPlaybackSession() {
         Log("Could not create cdm_private store directory: 0x%X.", GetLastError());
     }
 
-    // Create the key session with the given content decryption modules.
     BSTR keySystem = L"com.microsoft.playready";
-    
-    IMFMediaKeys *prKeys = nullptr;
-    if (FAILED(hr = keyFactory2->CreateMediaKeys2(keySystem, defaultCdmStorePath, inprivateCdmStorePath, &prKeys))) {
-        Log("Failed to create media keys: 0x%X", hr);
-        //return hr;
+
+    IMFMediaEngineClassFactory3 *keyFactory3 = nullptr;
+    if (FAILED(hr = factory->QueryInterface(__uuidof(IMFMediaEngineClassFactory3), (void**)&keyFactory3))) {
+        Log("Failed to query interface for IMFMediaEngineClassFactory3: %.2x", hr);
+        return hr;
     }
 
-    keyFactory2->Release();
+    /*
+     * Create initialization props array
+     *
+     * See: https://w3c.github.io/encrypted-media/#dom-mediakeysystemconfiguration
+     [{
+        initDataTypes: ['keyids', 'cenc'],
+        audioCapabilities: [{ contentType: 'audio/mp4; codecs="mp4a"' }],
+        videoCapabilities: [{ contentType: 'video/mp4; codecs="avc1"' }]
+     }]
+    */
+
+    IPropertyStore *props[4];
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&props[0]));
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&props[1]));
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&props[2]));
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&props[3]));
+
+    PROPVARIANT initDataTypes;
+    hr = InitPropVariantFromStringAsVector(L"keyids; cenc", &initDataTypes);
+    props[0]->SetValue(MF_EME_INITDATATYPES, initDataTypes);
+    PropVariantClear(&initDataTypes);
+
+    // Video
+    IPropertyStore *videoCaps;
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&videoCaps));
+    PROPVARIANT contentType;
+    hr = InitPropVariantFromString(L"video/mp4; codecs=\"avc1\"", &contentType);
+    videoCaps->SetValue(MF_EME_CONTENTTYPE, contentType);
+    PropVariantClear(&contentType);
+
+    PROPVARIANT videoCapabilitiesPropVar;
+    PropVariantInit(&videoCapabilitiesPropVar);
+    videoCapabilitiesPropVar.punkVal = videoCaps;
+    videoCapabilitiesPropVar.vt = VT_ARRAY;
+
+    PROPVARIANT videoCapabilities;
+    hr = InitPropVariantFromPropVariantVectorElem(videoCapabilitiesPropVar, 1, &videoCapabilities);
+    props[1]->SetValue(MF_EME_VIDEOCAPABILITIES, videoCapabilities);
+    PropVariantClear(&videoCapabilities);
+
+    // Audio
+    IPropertyStore *audioCapsStore;
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&audioCapsStore));
+    PROPVARIANT audioContentType;
+    hr = InitPropVariantFromString(L"audio/mp4", &audioContentType);
+    audioCapsStore->SetValue(MF_EME_CONTENTTYPE, audioContentType);
+    PropVariantClear(&contentType);
+
+    PROPVARIANT audioCapabilitiesPropVar;
+    PropVariantInit(&audioCapabilitiesPropVar);
+    audioCapabilitiesPropVar.punkVal = audioCapsStore;
+    audioCapabilitiesPropVar.vt = VT_ARRAY;
+
+    PROPVARIANT audioCapabilities;
+    hr = InitPropVariantFromPropVariantVectorElem(audioCapabilitiesPropVar, 1, &audioCapabilities);
+    props[2]->SetValue(MF_EME_AUDIOCAPABILITIES, audioCapabilities);
+    PropVariantClear(&audioCapabilities);
+
+    // Persistent state - do we need this?
+    PROPVARIANT persistentState;
+    hr = InitPropVariantFromString(L"required", &persistentState);
+    props[3]->SetValue(MF_EME_PERSISTEDSTATE, persistentState);
+    PropVariantClear(&persistentState);
+
+    IMFMediaKeySystemAccess *keyAccess = nullptr;
+    if (FAILED(hr = keyFactory3->CreateMediaKeySystemAccess(keySystem, props, 3, &keyAccess))) {
+        Log("CreateMediaKeySystemAccess failed: %.2x", hr);
+        return hr;
+    }
+
+    props[0]->Release();
+    props[1]->Release();
+    props[2]->Release();
+    props[3]->Release();
+
+    /*
+     * Media keys stuff.
+     */
+    IPropertyStore *cdmValues;
+    hr = PSCreateMemoryPropertyStore(IID_PPV_ARGS(&cdmValues));
+
+    // MF_EME_CDM_INPRIVATESTOREPATH
+    PROPVARIANT inPrivateStorePath;
+    hr = InitPropVariantFromString(inprivateCdmStorePath, &inPrivateStorePath);
+    cdmValues->SetValue(MF_EME_CDM_INPRIVATESTOREPATH, inPrivateStorePath);
+    PropVariantClear(&inPrivateStorePath);
+
+    // MF_EME_CDM_STOREPATH
+    PROPVARIANT storePath;
+    hr = InitPropVariantFromString(defaultCdmStorePath, &storePath);
+    cdmValues->SetValue(MF_EME_CDM_STOREPATH, storePath);
+    PropVariantClear(&storePath);
+
+    if (FAILED(hr = keyAccess->CreateMediaKeys(cdmValues, &mediaKeys))) {
+        Log("CreateMediaKeys failed: %.2x", hr);
+        return hr;
+    }
+
+    IMFMediaEngineEME *mediaEngineEME;
+    if (FAILED(hr = mediaEngine->QueryInterface(__uuidof(IMFMediaEngineEME), (void**)&mediaEngineEME))) {
+        Log("Failed to query interface for IMFMediaEngineEME 0x%x", hr);
+        return hr;
+    }
+    // TODO: Is this the best place to do this?
+    if (FAILED(hr = mediaEngineEME->SetMediaKeys(mediaKeys))) {
+        Log("Failed to set media keys 0x%X", hr);
+    }
+
+    mediaEngineEME->Release();
 
     BSTR url = SysAllocStringLen(streamManifestUrl.data(), streamManifestUrl.size());
     hr = mediaEngine->SetSource(url);
@@ -233,19 +417,6 @@ HRESULT CreateProtectedPlaybackSession() {
         Log("Failed to set media engine source %.2x", hr);
         return hr;
     }
-
-    IMFMediaEngineEME *mediaEngineEME;
-    if (FAILED(hr = mediaEngine->QueryInterface(__uuidof(IMFMediaEngineEME), (void**)&mediaEngineEME))) {
-        Log("Failed to query interface for IMFMediaEngineEME 0x%x", hr);
-        return hr;
-    }
-
-    // TODO: Is this the best place to do this?
-    if (FAILED(hr = mediaEngineEME->SetMediaKeys(prKeys))) {
-        Log("Failed to set media keys 0x%X", hr);
-    }
-
-    mediaEngineEME->Release();
 
     return S_OK;
 }
